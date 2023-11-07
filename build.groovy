1ece241f0a9711b92fe1776820ec856455aaa401
Eder Gillian Silva Beserra <142837887+edergillian-eero@users.noreply.github.com>
2023-11-07 17:40:58 -0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,21 +1,13 @@
 HARDWARES = ["test", "echo", "ball" ]
 VALID_HARDWARES = "all" + HARDWARES
 
-def numChangedFiles = 0
-def changeLogSets = currentBuild.changeSets
-for (int i = 0; i < changeLogSets.size(); i++) {
-  def entries = changeLogSets[i].items
-  for (int j = 0; j < entries.length; j++) {
-    def entry = entries[j]
-    def files = new ArrayList(entry.affectedFiles)
-    for (int k = 0; k < files.size(); k++) {
-      def file = files[k]
-      println file.path
-      if (! file.path.startsWith("scripts/release/")) {
-        numChangeFiles = numChangedFiles + 1
-      }
-    }
-  }
+def runPipeline() {
+   CHANGE_SET = sh (
+      script: 'git log -2 --name-only --oneline --pretty="format:"',
+      returnStdout: true
+   ).trim()
+   echo "Current changeset: ${CHANGE_SET}"
+   return (CHANGE_SET !=~ "scripts/release/(.*)")
 }
 
 def tokenizeAndTrim(string, token) {
@@ -44,7 +36,7 @@ def validateParam(param, valid) {
     return items
 }
 
-if (numChangedFiles > 0) {
+if (runPipeline()) {
     node('master') {
         properties([
             parameters([
